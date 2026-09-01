8065d92a8845d113172253e4405cdb6628a89555
sun111902 <gkdid0907@naver.com>
2026-09-01 17:51:54 +0900

    260901-swiper 버튼이동

---
+++ js/book.js
@@ -0,0 +1,71 @@
+// new section tabmenu
+const tabItems = document.querySelectorAll('#booktab li');
+const tabs = document.querySelectorAll('.books > div');
+
+tabItems.forEach((tab, i) => {
+    tab.addEventListener('click', () => {
+        // 탭에 해당하는 리스트 보이고, 나머지는 숨기기
+        tabs.forEach((tab, j) => {
+            tab.style.display = (i === j) ? 'flex' : 'none';
+        });
+    });
+});
+
+//new section books
+async function fetchBooks(query) {
+    const REST_API_KEY = "354a663e6079cbd2ca6b0101277b3592"
+    const params = new URLSearchParams({
+        query,
+    });
+    const url = `https://dapi.kakao.com/v3/search/book?${params}`;
+
+    const response = await fetch(url, {
+        method: 'GET',
+        headers: {
+            Authorization: `KakaoAK ${REST_API_KEY}`
+        }
+    });
+
+    if (!response.ok) {
+        throw new Error(`HTTP 오류: ${response.status}`);
+    }
+
+    return response.json();
+}
+
+async function bookData() {
+    
+    try {
+        const queries = [
+            { query: "소설", sectionId: "novel" },
+            { query: "기욤 뮈소", sectionId: "person" },
+            { query: "원피스", sectionId: "cartoon" },
+            { query: "종교", sectionId: "religion" },
+            { query: "불교", sectionId: "oldbook" },
+        ];
+
+        for (const { query, sectionId } of queries) {
+            const data = await fetchBooks(query);
+console.log(data)
+            // 해당 섹션 내의 .box 요소 8개 선택
+            const section = document.querySelector(`#${sectionId}`);
+            const boxElements = section.querySelectorAll(".book");
+
+            boxElements.forEach((box, i) => {
+                const doc = data.documents[i];
+                if (!doc) return;
+
+                // 요소 생성 및 추가
+                box.innerHTML = `<img src="${doc.thumbnail}">
+                        <h3>${doc.title}</h3>
+                        <p>${doc.price}</p>
+                        <p>${doc.sale_price.toLocaleString()}원</p>
+                        `
+            });
+        }
+    } catch (error) {
+        console.log('에러발생', error);
+    }
+}
+
+bookData();
